a5551873f02388092fa30890ace824cdb8fb904b
HoangDepTraiBoDoiThe <Mydrawingartist@gmail.com>
2024-06-10 19:04:38 +0700

    Add dependencies

--- Source/Project_MOBA/Project_MOBA.Build.cs
+++ Source/Project_MOBA/Project_MOBA.Build.cs
@@ -8,7 +8,7 @@ public class Project_MOBA : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayAbilitiesEditor", "GameplayAbilities", "EnhancedInput", "GameplayTasks", "GameplayTags", "UMG", "AIModule", "Niagara", "NavigationSystem" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
@@ -16,7 +16,7 @@ public class Project_MOBA : ModuleRules
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
 		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+		PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 	}
