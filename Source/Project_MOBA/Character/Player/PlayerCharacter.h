22ae73602219bb4daeb102c9b0e9e124409287db
HoangDepTraiBoDoiThe <Mydrawingartist@gmail.com>
2024-06-10 19:50:56 +0700

    Initialize base character and player character classes

---
+++ Source/Project_MOBA/Character/Player/PlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Project_MOBA/Character/BaseCharacter.h"
+#include "PlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_MOBA_API APlayerCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
