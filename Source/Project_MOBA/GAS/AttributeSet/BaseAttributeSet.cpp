a2c0ca5858ed4ea252766947e31ee937aa84c3c1
HoangDepTraiBoDoiThe <Mydrawingartist@gmail.com>
2024-06-11 21:31:00 +0700

    Add game attributes
    4 vital attrs, 5 primary attrs, 8 secondary attrs

--- Source/Project_MOBA/GAS/AttributeSet/BaseAttributeSet.cpp
+++ Source/Project_MOBA/GAS/AttributeSet/BaseAttributeSet.cpp
@@ -12,12 +12,113 @@ UBaseAttributeSet::UBaseAttributeSet()
 
 void UBaseAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
-	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
-
+	// Vital attributes
 	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, HitPoint, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, MaxHitPoint, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+
+	// Primary attributes
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, Vigor, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, Resilient, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, Resistance, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, Strength, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, Intelligence, COND_None, REPNOTIFY_Always);
+
+	// Secondary attributes
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, HitPointRegeneration, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, ManaRegeneration, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, Armor, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, MagicResist, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, ArmorPenetration, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, MagicResistPenetration, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, CriticalHitChange, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, BlockChange, COND_None, REPNOTIFY_Always);
 }
 
+#pragma region Rep Notify Functions
 void UBaseAttributeSet::OnRep_HitPoint(const FGameplayAttributeData& OldValue) const
 {
-	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, HitPoint, OldValue)
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, HitPoint, OldValue);
 }
+
+void UBaseAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldValue) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, Mana, OldValue); 
+}
+
+void UBaseAttributeSet::OnRep_MaxHitPoint(const FGameplayAttributeData& OldValue) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, MaxHitPoint, OldValue); 
+}
+
+void UBaseAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldValue) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, MaxMana, OldValue); 
+}
+
+void UBaseAttributeSet::OnRep_Vigor(const FGameplayAttributeData& OldValue) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, Vigor, OldValue); 
+}
+
+void UBaseAttributeSet::OnRep_Resilient(const FGameplayAttributeData& OldValue) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, Resilient, OldValue); 
+}
+
+void UBaseAttributeSet::OnRep_Resistance(const FGameplayAttributeData& OldValue) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, Resistance, OldValue); 
+}
+
+void UBaseAttributeSet::OnRep_Strength(const FGameplayAttributeData& OldValue) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, Strength, OldValue); 
+}
+
+void UBaseAttributeSet::OnRep_Intelligence(const FGameplayAttributeData& OldValue) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, Intelligence, OldValue); 
+}
+
+void UBaseAttributeSet::OnRep_HitPointRegeneration(const FGameplayAttributeData& OldValue) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, HitPointRegeneration, OldValue); 
+}
+
+void UBaseAttributeSet::OnRep_ManaRegeneration(const FGameplayAttributeData& OldValue) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, ManaRegeneration, OldValue); 
+}
+
+void UBaseAttributeSet::OnRep_Armor(const FGameplayAttributeData& OldValue) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, Armor, OldValue); 
+}
+
+void UBaseAttributeSet::OnRep_MagicResist(const FGameplayAttributeData& OldValue) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, MagicResist, OldValue);
+}
+
+void UBaseAttributeSet::OnRep_ArmorPenetration(const FGameplayAttributeData& OldValue) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, ArmorPenetration, OldValue);
+}
+
+void UBaseAttributeSet::OnRep_MagicResistPenetration(const FGameplayAttributeData& OldValue) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, MagicResistPenetration, OldValue); 
+}
+
+void UBaseAttributeSet::OnRep_CriticalHitChange(const FGameplayAttributeData& OldValue) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, CriticalHitChange, OldValue); 
+}
+
+void UBaseAttributeSet::OnRep_BlockChange(const FGameplayAttributeData& OldValue) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, BlockChange, OldValue);
+}
+#pragma endregion
--- Source/Project_MOBA/GAS/AttributeSet/BaseAttributeSet.h
+++ Source/Project_MOBA/GAS/AttributeSet/BaseAttributeSet.h
@@ -24,15 +24,119 @@ class PROJECT_MOBA_API UBaseAttributeSet : public UAttributeSet
 public:
 	UBaseAttributeSet();
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	// Vital attributes
 	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, HitPoint)
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, Mana)
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, MaxHitPoint)
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, MaxMana)
+
+	// Primary attributes
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, Vigor)
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, Resilient)
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, Resistance)
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, Strength)
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, Intelligence)
+
+	// Secondary attributes
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, HitPointRegeneration)
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, ManaRegeneration)
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, Armor)
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, MagicResist)
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, ArmorPenetration)
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, MagicResistPenetration)
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, CriticalHitChange)
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, BlockChange)
 
 protected:
 
 private:
+	// Vital attributes
 	UFUNCTION()
 	void OnRep_HitPoint(const FGameplayAttributeData& OldValue) const;
-	
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldValue) const;
+	UFUNCTION()
+	void OnRep_MaxHitPoint(const FGameplayAttributeData& OldValue) const;
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldValue) const;
+
+	// Primary attributes
+	UFUNCTION()
+	void OnRep_Vigor(const FGameplayAttributeData& OldValue) const;
+	UFUNCTION()
+	void OnRep_Resilient(const FGameplayAttributeData& OldValue) const;
+	UFUNCTION()
+	void OnRep_Resistance(const FGameplayAttributeData& OldValue) const;
+	UFUNCTION()
+	void OnRep_Strength(const FGameplayAttributeData& OldValue) const;
+	UFUNCTION()
+	void OnRep_Intelligence(const FGameplayAttributeData& OldValue) const;
+
+	// Secondary attributes
+	UFUNCTION()
+	void OnRep_HitPointRegeneration(const FGameplayAttributeData& OldValue) const;
+	UFUNCTION()
+	void OnRep_ManaRegeneration(const FGameplayAttributeData& OldValue) const;
+	UFUNCTION()
+	void OnRep_Armor(const FGameplayAttributeData& OldValue) const;
+	UFUNCTION()
+	void OnRep_MagicResist(const FGameplayAttributeData& OldValue) const;
+	UFUNCTION()
+	void OnRep_ArmorPenetration(const FGameplayAttributeData& OldValue) const;
+	UFUNCTION()
+	void OnRep_MagicResistPenetration(const FGameplayAttributeData& OldValue) const;
+	UFUNCTION()
+	void OnRep_CriticalHitChange(const FGameplayAttributeData& OldValue) const;
+	UFUNCTION()
+	void OnRep_BlockChange(const FGameplayAttributeData& OldValue) const;
+
+
+
+#pragma region Vital attributes
 	UPROPERTY(ReplicatedUsing=OnRep_HitPoint)
 	FGameplayAttributeData HitPoint;
+	UPROPERTY(ReplicatedUsing=OnRep_Mana)
+	FGameplayAttributeData Mana;
+	UPROPERTY(ReplicatedUsing=OnRep_MaxHitPoint)
+	FGameplayAttributeData MaxHitPoint;
+	UPROPERTY(ReplicatedUsing=OnRep_MaxMana)
+	FGameplayAttributeData MaxMana;
+#pragma endregion
+
+#pragma region Primary attributes
+	UPROPERTY(ReplicatedUsing=OnRep_Vigor)
+	FGameplayAttributeData Vigor;
+	UPROPERTY(ReplicatedUsing=OnRep_Resilient)
+	FGameplayAttributeData Resilient;
+	UPROPERTY(ReplicatedUsing=OnRep_Resistance)
+	FGameplayAttributeData Resistance;
+	UPROPERTY(ReplicatedUsing=OnRep_Strength)
+	FGameplayAttributeData Strength;
+	UPROPERTY(ReplicatedUsing=OnRep_Intelligence)
+	FGameplayAttributeData Intelligence;
+#pragma endregion
+
+#pragma region Secondary attributes
+	UPROPERTY(ReplicatedUsing=OnRep_HitPointRegeneration)
+	FGameplayAttributeData HitPointRegeneration;
+	UPROPERTY(ReplicatedUsing=OnRep_ManaRegeneration)
+	FGameplayAttributeData ManaRegeneration;
+	UPROPERTY(ReplicatedUsing=OnRep_Armor)
+	FGameplayAttributeData Armor;
+	UPROPERTY(ReplicatedUsing=OnRep_MagicResist)
+	FGameplayAttributeData MagicResist;
+	UPROPERTY(ReplicatedUsing=OnRep_ArmorPenetration)
+	FGameplayAttributeData ArmorPenetration;
+	UPROPERTY(ReplicatedUsing=OnRep_MagicResistPenetration)
+	FGameplayAttributeData MagicResistPenetration;
+	UPROPERTY(ReplicatedUsing=OnRep_CriticalHitChange)
+	FGameplayAttributeData CriticalHitChange;
+	UPROPERTY(ReplicatedUsing=OnRep_BlockChange)
+	FGameplayAttributeData BlockChange;
+#pragma endregion
 	
+#pragma region Meta attributes
+	FGameplayAttributeData XPIncoming;
+	FGameplayAttributeData DamageIncoming;
+#pragma endregion
 };
