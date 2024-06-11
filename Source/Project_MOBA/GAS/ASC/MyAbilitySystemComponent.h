94294a456db7049377bb7a07dfa5060d01c1b8b1
HoangDepTraiBoDoiThe <Mydrawingartist@gmail.com>
2024-06-11 15:08:17 +0700

    Initial GAS classes
    ASC, AS

---
+++ Source/Project_MOBA/GAS/ASC/MyAbilitySystemComponent.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "MyAbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_MOBA_API UMyAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+
+public:
+
+protected:
+
+private:
+
+	
+};
---
+++ Source/Project_MOBA/GAS/AttributeSet/BaseAttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "BaseAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_MOBA_API UBaseAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
