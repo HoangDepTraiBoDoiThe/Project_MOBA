0e7db453db5d644c8594e469cd99f96fbd0965ac
HoangDepTraiBoDoiThe <Mydrawingartist@gmail.com>
2024-06-17 15:02:16 +0700

    Initialize Minion classes and BPs

---
+++ Source/Project_MOBA/Character/Minion/Controller/MyAIController.h
@@ -0,0 +1,17 @@
+// Cu Nhat Hoang
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "MyAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_MOBA_API AMyAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Project_MOBA/Character/Minion/MinionCharacter.h
@@ -0,0 +1,17 @@
+// Cu Nhat Hoang
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Project_MOBA/Character/BaseCharacter.h"
+#include "MinionCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_MOBA_API AMinionCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
